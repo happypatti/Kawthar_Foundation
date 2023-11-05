12dc7f8024c68b515f5d7feebfe6e1c91ec5919d
Ali Shalash <aashalash40@gmail.com>
2023-11-04 20:05:08 -0400

    dd

--- app/googleAds.tsx
+++ app/googleAds.tsx
@@ -5,11 +5,14 @@ export default function GoogleAds() {
 return (
   <>
   <Script
+  id="gtag-manager"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script 
+      id="gtag-init"
+      strategy="lazyOnload">
         {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
