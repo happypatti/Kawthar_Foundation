89ae28ba470bd292d35289cb23b93f1a8437acd5
Ali Shalash <aashalash40@gmail.com>
2024-05-14 02:21:47 -0400

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,23 +4,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     {
-      url: "https://pixelperfectky.com",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://pixelperfectky.com/websites",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://pixelperfectky.com/seo",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://pixelperfectky.com/digital-marketing",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://pixelperfectky.com/about-us",
+      url: "https://kawtharfoundation.org",
       lastModified: new Date(),
     },
   ];
