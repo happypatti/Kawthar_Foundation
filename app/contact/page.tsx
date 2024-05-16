a67c1f0d202a94974e56629fd21e1932279ed94b
happypatti <43590094+happypatti@users.noreply.github.com>
2024-05-16 00:06:23 -0400

    Delete app/contact/page.tsx

--- app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import ContactUs from "@/components/shared/contact-us";
-
-export default async function Home() {
-  return (
-    <div className="z-10 flex flex-col items-center justify-center">
-      <ContactUs />
-    </div>
-  );
-}
